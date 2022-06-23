0c7e1e8880d5bfc5fadcbf62edaded8be11809ad
cyber-baron <44nikolaeva@mail.ru>
2022-06-23 13:12:59 +0300

    added connections between entities

--- src/main/java/com/example/springhelloworld/entity/Book.java
+++ src/main/java/com/example/springhelloworld/entity/Book.java
@@ -4,14 +4,29 @@ package com.example.springhelloworld.entity;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Data
-@Table(name="Книга")
+@Table(name="Book")
 public class Book {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name="BOOK_ID", nullable=false, unique=true)
     public int bookId;
+
+    @Column(name="BOOK_NAME", nullable=false, unique=false)
     private char bookName;
+    @Column(name="BOOK_PUBLISH_DATE", nullable=false, unique=false)
     private int bookPublishDate;
+
+    @ManyToMany(mappedBy = "bookAuthors")
+    Set<AuthorsBase> author;
+
+    @ManyToMany(mappedBy = "bookGenres")
+    Set<GenresBase> genre;
+
+    @OneToMany (targetEntity = BookOrder.class, cascade = CascadeType.ALL, mappedBy = "book")
+    private List<BookOrder> bookOrdersList;
 }
--- src/main/java/com/example/springhelloworld/entity/GenresBase.java
+++ src/main/java/com/example/springhelloworld/entity/GenresBase.java
@@ -3,14 +3,24 @@ package com.example.springhelloworld.entity;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @Data
-@Table(name="База Жанров")
+@Table(name="Genres Base")
 public class GenresBase {
 
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name="GENRE_ID", nullable=false, unique=true)
     public int genreId;
+
+    @Column(name="GENRE_NAME", nullable=false, unique=false)
     private String genreName;
+
+    @ManyToMany
+    @JoinTable(
+            name = "Genres Base",
+            joinColumns = @JoinColumn(name = "GENRE_ID"))
+    Set<Book> bookGenres;
 }
